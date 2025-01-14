e1db7909c3216053fa9e8e1f3f584aeba5c0054c
Arthur <anon121213@mail.ru>
2025-01-15 02:44:03 +0300

    [fix] frontend links

--- backend/Bot/bot.js
+++ backend/Bot/bot.js
@@ -1,56 +1,64 @@
-require('dotenv').config();
+require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 
-const TOKEN = process.env.TELEGRAM_BOT_TOKEN; 
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const GAME_URL = process.env.GAME_URL;
 const bot = new TelegramBot(TOKEN, { polling: true });
 const gameName = "fridgeTestGame";
 
 bot.onText(/help/, (msg) => {
-    bot.sendMessage(msg.from.id, "Say /game if you want to play.");
+  bot.sendMessage(msg.from.id, "Say /game if you want to play.");
 });
 
 bot.onText(/start|game/, (msg) => {
-    const gameurl = `${GAME_URL}?tgId=${msg.from.id}`;
-    bot.sendGame(msg.from.id, gameName).catch((error) => {
-        console.error("Error sending game:", error);
-    });
-    bot.answerCallbackQuery({
-        callback_query_id: msg.message_id,
-        url: gameurl
-    }).catch((error) => {
-        console.error("Error responding to callback query:", error);
+  const gameurl = `${GAME_URL}?tgId=${msg.from.id}`;
+  bot.sendGame(msg.from.id, gameName).catch((error) => {
+    console.error("Error sending game:", error);
+  });
+  bot
+    .answerCallbackQuery({
+      callback_query_id: msg.message_id,
+      url: gameurl,
+    })
+    .catch((error) => {
+      console.error("Error responding to callback query:", error);
     });
 });
 
 bot.on("callback_query", (query) => {
-    if (query.game_short_name !== gameName) {
-        bot.answerCallbackQuery({
-            callback_query_id: query.id,
-            text: `Sorry, '${query.game_short_name}' is not available.`,
-        }).catch((error) => {
-            console.error("Error responding to callback query:", error);
-        });
-    } else {
-        const gameurl = `${GAME_URL}?initData=${query.data}`;
-        bot.answerCallbackQuery({
-            callback_query_id: query.id,
-            url: gameurl,
-        }).catch((error) => {
-            console.error("Error responding to callback query:", error);
-        });
-    }
+  if (query.game_short_name !== gameName) {
+    bot
+      .answerCallbackQuery({
+        callback_query_id: query.id,
+        text: `Sorry, '${query.game_short_name}' is not available.`,
+      })
+      .catch((error) => {
+        console.error("Error responding to callback query:", error);
+      });
+  } else {
+    const gameurl = `${GAME_URL}?initData=${query.data}`;
+    bot
+      .answerCallbackQuery({
+        callback_query_id: query.id,
+        url: gameurl,
+      })
+      .catch((error) => {
+        console.error("Error responding to callback query:", error);
+      });
+  }
 });
 
 bot.on("inline_query", (iq) => {
-    bot.answerInlineQuery(iq.id, [
-        {
-            type: "game",
-            id: "0",
-            game_short_name: gameName,
-        },
-    ]).catch((error) => {
-        console.error("Error responding to inline query:", error);
+  bot
+    .answerInlineQuery(iq.id, [
+      {
+        type: "game",
+        id: "0",
+        game_short_name: gameName,
+      },
+    ])
+    .catch((error) => {
+      console.error("Error responding to inline query:", error);
     });
 });
 
--- backend/Server/server.js
+++ backend/Server/server.js
@@ -1,45 +1,46 @@
-require('dotenv').config(); 
+require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const { Client } = require('pg');
-const crypto = require('crypto');
+const { Client } = require("pg");
+const crypto = require("crypto");
 
-const sha256 = (data) => crypto.createHash('sha256').update(data).digest('hex');
+const sha256 = (data) => crypto.createHash("sha256").update(data).digest("hex");
 
 const server = express();
 const port = process.env.PORT || 3000;
 const secretKey = process.env.JWT_SECRET;
-const botSecretKey = process.env.BOT_SECRET_KEY; 
+const botSecretKey = process.env.BOT_SECRET_KEY;
 
-console.log("POSTGRES_USER:", process.env.POSTGRES_USER); 
+console.log("POSTGRES_USER:", process.env.POSTGRES_USER);
 console.log("POSTGRES_PASSWORD:", process.env.POSTGRES_PASSWORD);
 
 const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-client.connect()
-    .then(() => console.log('Connected to PostgreSQL'))
-    .catch(err => console.error('Connection error', err.stack));
+client
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch((err) => console.error("Connection error", err.stack));
 
 async function executeQuery(query, params = []) {
-    try {
-        const res = await client.query(query, params);
-        return res.rows;
-    } catch (err) {
-        console.error('Error executing query:', err.stack);
-        throw err;
-    }
+  try {
+    const res = await client.query(query, params);
+    return res.rows;
+  } catch (err) {
+    console.error("Error executing query:", err.stack);
+    throw err;
+  }
 }
 
 server.use(cors());
@@ -47,114 +48,115 @@ server.use(express.json());
 server.use(express.static(path.join(__dirname, "FridgeHost")));
 
 function generateToken(payload) {
-    return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  return jwt.sign(payload, secretKey, { expiresIn: "1h" });
 }
 
 function verifyToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) {
-        return res.status(403).send("A token is required for authentication");
-    }
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
 
-    try {
-        const decoded = jwt.verify(token, secretKey);
-        req.user = decoded;
-    } catch (err) {
-        return res.status(401).send("Invalid Token");
-    }
-    return next();
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
 }
 
-server.get('/GetJwt', (req, res) => {
-    const authorizationHeader = req.headers['authorization'];
+server.get("/GetJwt", (req, res) => {
+  const authorizationHeader = req.headers["authorization"];
 
-    if (!authorizationHeader) {
-        return res.status(400).json({ error: 'Authorization header is missing' });
-    }
+  if (!authorizationHeader) {
+    return res.status(400).json({ error: "Authorization header is missing" });
+  }
 
-    const initDataRaw = authorizationHeader; 
-    const initData = initDataRaw.replace(/tg_hash=[^&]*&?/, ''); // Удаляем tg_hash из initData
-    const tgHash = authorizationHeader.match(/tg_hash=([^&]*)/)[1]; // Извлекаем tg_hash
+  const initDataRaw = authorizationHeader;
+  const initData = initDataRaw.replace(/tg_hash=[^&]*&?/, ""); // Удаляем tg_hash из initData
+  const tgHash = authorizationHeader.match(/tg_hash=([^&]*)/)[1]; // Извлекаем tg_hash
 
-    const computedHash = sha256(initData + botSecretKey); // Вычисляем хэш с bot_secret_key
+  const computedHash = sha256(initData + botSecretKey); // Вычисляем хэш с bot_secret_key
 
-    if (computedHash !== tgHash) {
-        return res.status(401).json({ error: 'Invalid hash' });
-    }
+  if (computedHash !== tgHash) {
+    return res.status(401).json({ error: "Invalid hash" });
+  }
 
-    const jwtToken = generateToken({ hash: computedHash });  // Генерация JWT на основе данных
+  const jwtToken = generateToken({ hash: computedHash }); // Генерация JWT на основе данных
 
-    res.json({ jwt: jwtToken });
+  res.json({ jwt: jwtToken });
 });
 
 // Использование JWT для авторизации
 server.get("/auth", async (req, res) => {
-    const authHeader = req.headers['authorization'];
+  const authHeader = req.headers["authorization"];
 
-    if (!authHeader) {
-        return res.status(400).send("Authorization header is missing");
+  if (!authHeader) {
+    return res.status(400).send("Authorization header is missing");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    const hash = decoded.hash; // Получаем хэш из декодированного токена
+
+    // Теперь, вместо tgId, используем hash для поиска в базе данных
+    const query = "SELECT * FROM users WHERE hash = $1";
+    const params = [hash];
+
+    const users = await executeQuery(query, params);
+    if (users.length > 0) {
+      // Пользователь найден, возвращаем token
+      res.status(200).json({ token });
+    } else {
+      // Новый пользователь, создаем запись в базе данных
+      const defaultData = JSON.stringify({ level: 0, money: 0 });
+      const insertQuery =
+        "INSERT INTO users (hash, save_data) VALUES ($1, $2) RETURNING *";
+      const newUser = await executeQuery(insertQuery, [hash, defaultData]);
+      res.status(201).json({ token });
     }
-
-    const token = authHeader.split(' ')[1];
-
-    try {
-        const decoded = jwt.verify(token, secretKey);
-        const hash = decoded.hash;  // Получаем хэш из декодированного токена
-
-        // Теперь, вместо tgId, используем hash для поиска в базе данных
-        const query = 'SELECT * FROM users WHERE hash = $1';
-        const params = [hash];
-
-        const users = await executeQuery(query, params);
-        if (users.length > 0) {
-            // Пользователь найден, возвращаем token
-            res.status(200).json({ token });
-        } else {
-            // Новый пользователь, создаем запись в базе данных
-            const defaultData = JSON.stringify({ level: 0, money: 0 });
-            const insertQuery = 'INSERT INTO users (hash, save_data) VALUES ($1, $2) RETURNING *';
-            const newUser = await executeQuery(insertQuery, [hash, defaultData]);
-            res.status(201).json({ token });
-        }
-    } catch (err) {
-        res.status(500).send("Error occurred during authorization.");
-    }
+  } catch (err) {
+    res.status(500).send("Error occurred during authorization.");
+  }
 });
 
 // API для сохранения данных
 server.get("/save", verifyToken, async (req, res) => {
-    const saveData = JSON.parse(req.query.saveData);
-    const hash = req.user.hash;  // Получаем хэш из декодированного токена
+  const saveData = JSON.parse(req.query.saveData);
+  const hash = req.user.hash; // Получаем хэш из декодированного токена
 
-    try {
-        const updateQuery = 'UPDATE users SET save_data = $1 WHERE hash = $2';
-        await executeQuery(updateQuery, [JSON.stringify(saveData), hash]);
-        res.status(200).send("Data saved successfully.");
-    } catch (err) {
-        res.status(500).send("Error occurred while saving data.");
-    }
+  try {
+    const updateQuery = "UPDATE users SET save_data = $1 WHERE hash = $2";
+    await executeQuery(updateQuery, [JSON.stringify(saveData), hash]);
+    res.status(200).send("Data saved successfully.");
+  } catch (err) {
+    res.status(500).send("Error occurred while saving data.");
+  }
 });
 
 // API для загрузки данных
 server.get("/load", verifyToken, async (req, res) => {
-    const hash = req.user.hash;  // Получаем хэш из декодированного токена
+  const hash = req.user.hash; // Получаем хэш из декодированного токена
 
-    try {
-        const query = 'SELECT save_data FROM users WHERE hash = $1';
-        const result = await executeQuery(query, [hash]);
+  try {
+    const query = "SELECT save_data FROM users WHERE hash = $1";
+    const result = await executeQuery(query, [hash]);
 
-        if (result.length > 0) {
-            res.status(200).json({ saveData: result[0].save_data });
-        } else {
-            res.status(404).send("User data not found.");
-        }
-    } catch (err) {
-        res.status(500).send("Error occurred while loading data.");
+    if (result.length > 0) {
+      res.status(200).json({ saveData: result[0].save_data });
+    } else {
+      res.status(404).send("User data not found.");
     }
+  } catch (err) {
+    res.status(500).send("Error occurred while loading data.");
+  }
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
